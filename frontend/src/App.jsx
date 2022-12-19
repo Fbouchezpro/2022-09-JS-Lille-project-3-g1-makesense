cc17ff4b8ec7929e7fe0cda2e191ce3401076403
Fbouchezpro <fbouchezpro@gmail.com>
2022-12-19 10:04:33 +0100

    =adding some path on react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import SugestCard from "@components/SugestCard/SugestCard";
 import Home from "@pages/Home/Home";
 import { Routes, Route } from "react-router-dom";
 
@@ -6,6 +7,11 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/suggest" element={<SugestCard />} />
+        <Route path="/historic" />
+        <Route path="/contacts" />
+        <Route path="/params" />
+        <Route path="/logout" />
       </Routes>
     </div>
   );
