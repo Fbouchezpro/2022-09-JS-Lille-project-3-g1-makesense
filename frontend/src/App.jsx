2dde6dc51c91a8a32c0ebe6671420ba1e44afcf6
sax59 <waquetv@gmail.com>
2022-12-19 11:08:25 +0100

    Add responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import Home from "@pages/Home/Home";
+import Suggest from "@pages/Suggest/Suggest";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/timeline" element={<Suggest />} />
       </Routes>
     </div>
   );
